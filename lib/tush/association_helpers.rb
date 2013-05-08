d2573dd8b9a087d07a16e7547a8d6cd783929c3e
Lauren Mermel <lauren@nationbuilder.com>
2013-05-08 15:01:47 -0700

    Reorganizing files

--- lib/tush/association_helpers.rb
+++ lib/tush/association_helpers.rb
@@ -1,30 +1,6 @@
 module Tush
-  class ModelStore
-
-    attr_accessor :model, :model_stack, :belongs_to, :has_many, :has_one
-
-    def initialize(model)
-      self.model = model
-      self.model_stack = []
-      self.has_many = {}
-      self.belongs_to = []
-      self.has_one = {}
-    end
-
-    def push_array_to_model_stack(model_array)
-      self.model_stack.concat model_array.map.{ |model_instance| 
-                                                ModelWrapper.new(model_instance) }
-    end
-
-    def push(model_instance)
-      model_stack.push ModelWrapper.new(model_instance)
-    end
-
-    def pop
-      model_stack.pop
-    end
-
-    def set_has_many
+  class AssociationHelpers
+    def self.set_has_many
       associations = model.reflect_on_all_associations(:has_many)
       if associations.any?
       classes = associations.map(&:class_name)
@@ -48,7 +24,5 @@ module Tush
         self.has_one = Hash[classes.zip(class_keys)]
       end 
     end
-
-
   end
 end
